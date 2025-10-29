975721ec6d481ca6d1dabe86652f10dad1de3f30
nanamiyoshida <136306418+nana-hina@users.noreply.github.com>
2025-10-29 17:26:15 +0900

    month.cpp

--- month.cpp
+++ month.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int month;
 
-    std::cout << "Input month: ";
-    std::cin >> month;
+    cout << "Input month: ";
+    cin >> month;
 
     if (month < 1 || month > 12) {
-        std::cout << "Invalid month." << std::endl;
+        cout << "Invalid month." << endl;
     } else if (month == 2) {
-        std::cout << "The month has 28 days." << std::endl;
+        cout << "The month has 28 days." << endl;
     } else if (month == 4 || month == 6 || month == 9 || month == 11) {
         // 小の月（30日まで）
-        std::cout << "The month has 30 days." << std::endl;
+        cout << "The month has 30 days." << endl;
     } else {
         // 大の月（31日まで）
-        std::cout << "The month has 31 days." << std::endl;
+        cout << "The month has 31 days." << endl;
     }
 
     return 0;
